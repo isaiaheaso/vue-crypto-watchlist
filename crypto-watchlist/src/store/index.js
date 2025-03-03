8606b93f7e9b921974bb4e7daaa84a9e797351ac
Isaiah Easo <isaiah@easo.io>
2025-03-03 11:02:04 -0600

    Reverting changes

--- crypto-watchlist/src/store/index.js
+++ crypto-watchlist/src/store/index.js
@@ -10,11 +10,6 @@ const store = new Vuex.Store({
         query,
         watchlist  
     },
-    state: {
-        apiBaseUrl: process.env.NODE_ENV === 'production' 
-            ? 'https://crypto-watchlist-api.onrender.com/api'
-            : 'http://localhost:3000/api'
-    },
 });
 
 export default store;
